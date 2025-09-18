f8f064654c45e263cd876aeb3837b09ca307e801
Anthony Lopez <anthonylopez1230@gmail.com>
2025-09-18 13:40:29 +0200

    Gsap animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,14 +1,46 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
 
 const Hero = () => {
-  const color = "#fff";
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const gsapContext = gsap.context(() => {
+      const timeline = gsap.timeline({ defaults: { ease: "power3.out" } });
+
+      timeline
+        .from(".line-1", {
+          y: 50,
+          opacity: 0,
+          duration: 0.8,
+        })
+        .from(".line-2", {
+          scale: 0.4,
+          opacity: 0,
+          duration: 1,
+        })
+        .from(".line-3", {
+          y: -50,
+          opacity: 0,
+          duration: 0.8,
+        })
+        .from(".cta-btn", {
+          y: 30,
+          opacity: 0,
+          duration: 0.6,
+        });
+    }, containerRef);
+
+    return () => gsapContext.revert();
+  }, []);
   return (
-    <section className="flex flex-col h-svh justify-center max-w-[1300px] mx-auto">
-      <h2 className="text-4xl pl-4">Let's build something</h2>
-      <h1 className="text-[160px] leading-none bg-linear-to-r from-[#fcad37] to-red-500/70 bg-clip-text text-transparent">
+    <section ref={containerRef} className="flex flex-col h-svh justify-center max-w-[1300px] mx-auto overflow-hidden">
+      <h2 className="line-1 text-4xl pl-4">Let's build something</h2>
+      <h1 className="line-2  text-[160px] leading-none bg-linear-to-r from-[#fcad37] to-red-500/70 bg-clip-text text-transparent">
         BIG
       </h1>
-      <h2 className="text-4xl pl-4">Together</h2>
+      <h2 className="line-3 text-4xl pl-4">Together</h2>
     </section>
   );
 };
--- components/HeroGlb.tsx
+++ components/HeroGlb.tsx
@@ -6,7 +6,7 @@ import React, { useRef } from "react";
 const Scene = () => {
   const meshRef = useRef(null) as any;
 
-  useFrame((state, delta) => {
+  useFrame(() => {
     if (meshRef.current) {
       meshRef.current.rotation.y += 0.001; // rota en Y
       meshRef.current.rotation.x += 0.0005; // opcional: rota también en X
@@ -17,7 +17,7 @@ const Scene = () => {
       <ambientLight intensity={1} />
       <OrbitControls enablePan={false} enableZoom={false} />
       <directionalLight position={[-1, 0, 5]} intensity={2} color={"red"} />
-      <mesh position={[0, 0, 0]} ref={meshRef}>
+      <mesh position={[0, 0, 0]} ref={meshRef} rotation={[Math.PI / 3, 0, 0]}>
         <sphereGeometry args={[1, 64, 64]} />
         <meshStandardMaterial color={"yellow"} metalness={1} roughness={0.4} wireframe />
       </mesh>
